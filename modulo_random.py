83f6321c314124be7c156586f5f6aa41b06f4c09
Luca Antonio Negrette <52060219+Luca13-cmyk@users.noreply.github.com>
2019-12-10 13:03:46 -0300

    Update modulo_random.py

--- modulo_random.py
+++ modulo_random.py
@@ -3,7 +3,7 @@ Módulo Random e o que são módulos?
 
 - Em Python, módulos nada mais são do que outros arquivos Python.
 
-Módulo Random -> Possui várias funções para geração de números pseudo-aleatório.
+Módulo Random -> Possui várias funções para geração de números pseudo-aleatório. (Pseudo -> Podem haver repeticoes)
 
 # OBS: Existem duas formas de se utilizar um módulo ou função deste
 
